7c438d443f6cfcc0a50c839cbec4861629d51ec2
saivishnu725 <saivishnu725@gmail.com>
2024-05-01 15:45:59 +0530

    TEMP hide other pages

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -1,18 +1,27 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './PageRouter.css';
+import StartPage from './pages/StartPage.js';
+{/*
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import UserProfile from './pages/UserProfile.js';
+import Settings from './pages/Settings.js';
+import NotFound from './pages/NotFound.js'; */}
 
 function PageRouter() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<StartPage />} />
+        {/*
         <Route exact path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user/:username" element={<UserProfile />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="*" element={<NotFound />} /> {/* This route is rendered if no other routes match */}
+        <Route path="*" element={<NotFound />} /> */} {/* This route is rendered if no other routes match */}
       </Routes>
     </Router>
   );

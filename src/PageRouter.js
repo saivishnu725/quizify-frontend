8296b033fd8ed44809373c78df36d6b8bf689350
saivishnu725 <saivishnu725@gmail.com>
2024-05-06 18:39:41 +0530

    EDIT uncomment route to /register

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -1,11 +1,10 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './PageRouter.css';
 import StartPage from './pages/StartPage.js';
+import Register from './pages/Register.js';
 {/*
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
-import Register from './pages/Register.js';
 import UserProfile from './pages/UserProfile.js';
 import Settings from './pages/Settings.js';
 import NotFound from './pages/NotFound.js'; */}
@@ -15,10 +14,10 @@ function PageRouter() {
     <Router>
       <Routes>
         <Route exact path="/" element={<StartPage />} />
+        <Route path="/register" element={<Register />} />
         {/*
         <Route exact path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/user/:username" element={<UserProfile />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<NotFound />} /> */} {/* This route is rendered if no other routes match */}

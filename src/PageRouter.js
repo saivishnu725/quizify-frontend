62783cdfef247d331bde72d980a3b3e91860d2fd
saivishnu725 <saivishnu725@gmail.com>
2024-05-06 19:20:44 +0530

    EDIT uncomment route to /Home

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './pages/StartPage.js';
 import Login from './pages/Login.js';
 import Register from './pages/Register.js';
-{/*
 import Home from './pages/Home.js';
+{/*
 import UserProfile from './pages/UserProfile.js';
 import Settings from './pages/Settings.js';
 import NotFound from './pages/NotFound.js'; */}
@@ -16,8 +16,8 @@ function PageRouter() {
         <Route exact path="/" element={<StartPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/*
         <Route exact path="/home" element={<Home />} />
+        {/*
         <Route path="/user/:username" element={<UserProfile />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<NotFound />} /> */} {/* This route is rendered if no other routes match */}

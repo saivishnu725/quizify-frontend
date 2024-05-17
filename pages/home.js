0f0787286d71b52aa215b42c1182e8f4413dfed7
saivishnu725 <saivishnu725@gmail.com>
2024-05-17 19:40:41 +0530

    ADD files to create site's structure

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+// pages/home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome Home!</h1>
+            {/* Other content */}
+        </div>
+    );
+};
+
+export default Home;

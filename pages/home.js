0a18f5eac9f8e324e4ddc07e94e92e009d68125f
saivishnu725 <saivishnu725@gmail.com>
2024-05-20 22:33:50 +0530

    TEMP some buttons to create the functionality before visual form

--- pages/home.js
+++ pages/home.js
@@ -4,9 +4,18 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Welcome Home!</h1>
             {/* Other content */}
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+
+                <h1>Welcome!</h1>
+                <button style={{ fontSize: '24px', padding: '10px 20px', marginBottom: '10px' }}>Create!</button>
+                <br />
+                <input type="text" placeholder="Enter the 6 digit code" style={{ marginBottom: '10px' }} />
+                <br />
+                <button style={{ fontSize: '18px', padding: '8px 16px' }}>Join</button>
+            </div>
         </div>
+        // </div>
     );
 };
 

f9edb0318bd13965857bc614d4c3fb5ff372043c
saivishnu725 <saivishnu725@gmail.com>
2024-05-26 20:02:52 +0530

    ADD tailwindcss (to use ShadCN)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './pages/components/**/*.{js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+

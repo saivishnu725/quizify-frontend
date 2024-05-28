8336a8ec93d7662a6e14292526d66457acfaf816
saivishnu725 <saivishnu725@gmail.com>
2024-05-28 20:34:41 +0530

    REMOVE unnecessary lines

--- pages/home.js
+++ pages/home.js
@@ -3,19 +3,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            {/* Other content */}
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
 
-                <h1>Welcome!</h1>
-                <button style={{ fontSize: '24px', padding: '10px 20px', marginBottom: '10px' }}>Create!</button>
-                <br />
-                <input type="text" placeholder="Enter the 6 digit code" style={{ marginBottom: '10px' }} />
-                <br />
-                <button style={{ fontSize: '18px', padding: '8px 16px' }}>Join</button>
-            </div>
+            <h1>Welcome!</h1>
+            <button style={{ fontSize: '24px', padding: '10px 20px', marginBottom: '10px' }}>Create!</button>
+            <br />
+            <input type="text" placeholder="Enter the 6 digit code" style={{ marginBottom: '10px' }} />
+            <br />
+            <button style={{ fontSize: '18px', padding: '8px 16px' }}>Join</button>
         </div>
-        // </div>
     );
 };
 

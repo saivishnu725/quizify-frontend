59e03637fa0a136c50fb4de3757e8b70723d6009
saivishnu725 <saivishnu725@gmail.com>
2024-06-07 16:44:52 +0530

    UPDATE indent/formatting fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import PropTypes from 'prop-types';
-import '../styles/globals.css'
+import PropTypes from "prop-types";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+
 MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
-}
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};

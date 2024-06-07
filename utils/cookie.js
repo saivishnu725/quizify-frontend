bb33898f2b4ee41fe521336e3fe92c83f03ac998
saivishnu725 <saivishnu725@gmail.com>
2024-06-07 16:45:49 +0530

    USE js-cookie to save and access token anytime

---
+++ utils/cookie.js
@@ -0,0 +1,12 @@
+import Cookies from "js-cookie";
+
+export const setToken = (token) => {
+  Cookies.set("tokey", token, { expires: 1 }); // Set token for 1 day
+};
+
+export const getToken = () => {
+  return Cookies.get("tokey");
+};
+export const removeToken = () => {
+  Cookies.remove("token");
+};
